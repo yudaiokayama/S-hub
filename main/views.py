42d8cfdb9a7c6bf0005f02d00267e2763815c0e4
ueffy <dentaku1909@ezweb.ne.jp>
2024-05-08 11:34:12 +0900

    backend commit 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def example_view(request):
+    if request.method == 'GET':
+        # GETリクエストの場合の処理
+        data = {'message': 'This is a GET request'}
+        return Response(data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        # POSTリクエストの場合の処理
+        received_data = request.data  # POSTされたデータを取得
+        # データを処理する他のコード...
+        return Response({'message': 'Data received successfully'}, status=status.HTTP_201_CREATED)
--- sys1/settings.py
+++ sys1/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'api.apps.SnsApiConfig',    #API用のアプリディレクトリ
+    'rest_framework',
+    'log',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#additional auth backends
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'log.auth.CSVBackend',
+]
